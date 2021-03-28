4cdbb54f851123d6c3885e8b9249cc91829ba52c
superfury <superfury@live.nl>
2021-03-28 13:32:36 +0200

    - Reverted the write mode 3 operation to a more simple method with less table lookups.

--- UniPCemu/hardware/vga/vga_mmu.c
+++ UniPCemu/hardware/vga/vga_mmu.c
@@ -266,7 +266,7 @@ uint_32 VGA_WriteMode3(uint_32 data) //Ignore enable set reset register!
 {
 	data = (byte)ror((byte)data, GETBITS(getActiveVGA()->registers->GraphicsRegisters.REGISTERS.DATAROTATEREGISTER,0,7)); //Rotate it! Keep 8-bit data!
 	data &= 0xFF; //Prevent overflow!
-	data = ALUMaskLatchOperation(getActiveVGA()->FillTable[GETBITS(getActiveVGA()->registers->GraphicsRegisters.REGISTERS.SETRESETREGISTER,0,0xF)], getActiveVGA()->ExpandTable[data]&getActiveVGA()->ExpandTable[getActiveVGA()->registers->GraphicsRegisters.REGISTERS.BITMASKREGISTER]); //Use the generated data on the Set/Reset register
+	data = ALUMaskLatchOperation(getActiveVGA()->FillTable[GETBITS(getActiveVGA()->registers->GraphicsRegisters.REGISTERS.SETRESETREGISTER,0,0xF)], getActiveVGA()->ExpandTable[data & getActiveVGA()->registers->GraphicsRegisters.REGISTERS.BITMASKREGISTER]); //Use the generated data on the Set/Reset register
 	return data;
 }
 
