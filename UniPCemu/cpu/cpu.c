74b91c4fc23e48955188fed9489610165343cf4a
superfury <superfury@live.nl>
2021-07-12 13:18:18 +0200

    - Improve CPU and APIC to always listen to INIT responses, no matter if it's from RESET or not.

--- UniPCemu/cpu/cpu.c
+++ UniPCemu/cpu/cpu.c
@@ -1414,14 +1414,14 @@ void resetCPU(word isInit) //Initialises the currently selected CPU!
 	{
 		APIC_updateWindowMSR(activeCPU, 0, 0); //Update the MSR for the hardware! Disable the APIC!
 	}
-	if ((isInit==0x80) && activeCPU) //INIT? Waiting for SIPI on non-BSP!
+	if ((isInit&0x80) && activeCPU) //INIT? Waiting for SIPI on non-BSP!
 	{
 		resetLAPIC(activeCPU, 2); //INIT reset of the APIC!
 		CPU[activeCPU].waitingforSIPI = 1; //Waiting!
 	}
 	else //Normal reset?
 	{
-		resetLAPIC(activeCPU, (isInit == 0x80)?2:1); //Hard reset of the APIC? Depends on INIT vs RESET!
+		resetLAPIC(activeCPU, (isInit&0x80)?2:1); //Hard reset of the APIC? Depends on INIT vs RESET!
 		//Make sure the local APIC is using the current values!
 		if (EMULATED_CPU >= CPU_PENTIUMPRO) //Has APIC support?
 		{
