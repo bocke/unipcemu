73289378e2335d9d721bb228a1b9fe9c80875a03
superfury <superfury@live.nl>
2021-09-08 09:46:06 +0200

    - Fixed PPP stream for unframed IPX packets.

--- UniPCemu/hardware/modem.c
+++ UniPCemu/hardware/modem.c
@@ -4884,10 +4884,7 @@ byte PPP_parseSentPacketFromClient(sword connectedclient, byte handleTransmit)
 			if (Packetserver_clients[connectedclient].ppp_IPXCPstatus[1] && Packetserver_clients[connectedclient].ppp_PAPstatus[1] && Packetserver_clients[connectedclient].ppp_LCPstatus[1]) //Fully authenticated and logged in for sending?
 			{
 				//Handle IPX frames in a special way!
-				if (!createPPPstream(&pppstream, &Packetserver_clients[connectedclient].packetserver_transmitbuffer[0], Packetserver_clients[connectedclient].packetserver_transmitlength)) //Create a substream for the information field?
-				{
-					goto ppp_finishpacketbufferqueue; //Finish up!
-				}
+				createPPPstream(&pppstream, &Packetserver_clients[connectedclient].packetserver_transmitbuffer[0], Packetserver_clients[connectedclient].packetserver_transmitlength); //Create a stream for the information field?
 				goto handlePPPIPXframe; //Handle the frame for IPX to use!
 			}
 		}
