b9adf715a9dc6715ab5dccc45558cc32fd5c2424
superfury <superfury@live.nl>
2021-09-17 23:38:55 +0200

    - Fixed the IPCP subnet address used for clients.

--- UniPCemu/hardware/modem.c
+++ UniPCemu/hardware/modem.c
@@ -1658,7 +1658,7 @@ byte sendpkt_pcap(PacketServer_clientp connectedclient, uint8_t* src, uint16_t l
 					{
 						memcpy(&srcip,((connectedclient->packetserver_slipprotocol == 3) && (!connectedclient->packetserver_slipprotocol_pppoe) && IPCP_OPEN) ? &connectedclient->ipcp_ipaddress[PPP_RECVCONF][0] : &connectedclient->packetserver_staticIP[0], 4); //The clients own IP address!
 						memcpy(&dstip, &src[sizeof(ethernetheader.data) + 16], 4); //The IP address!
-						if (((dstip&connectedclient->ipcp_subnetmaskipaddressd)==(srcip&connectedclient->ipcp_subnetmaskipaddrd)) && connectedclient->ipcp_subnetmaskipaddrd) //Local network destination?
+						if (((dstip&connectedclient->ipcp_subnetmaskipaddressd)==(srcip&connectedclient->ipcp_subnetmaskipaddressd)) && connectedclient->ipcp_subnetmaskipaddressd) //Local network destination?
 						{
 							memcpy(src, &maclocal, 6); //Send to ourselves for now!
 						}
